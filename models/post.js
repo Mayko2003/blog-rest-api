1a53276315ddbff83e9863bc04b24b4a9c371a75
Mayko2003 <maykox34f@gmail.com>
2023-01-10 21:48:14 -0300

    chore: init Post model

---
+++ models/post.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const Post = sequelize.define('Post', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.BIGINT
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.STRING(3000),
+            allowNull: false
+        },
+        slug: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        front_image: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        likes: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            defaultValue: 0
+        },
+        status: {
+            type: DataTypes.ENUM('published', 'draft'),
+            defaultValue: 'draft'
+        },
+        userId: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+        }
+    }, {
+        timestamps: false,
+    });
+
+    Post.associate = function (models) {
+    };
+    return Post;
+};
