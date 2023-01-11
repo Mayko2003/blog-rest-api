043bbc28c6a2fe129395077c23dd739a6e6020d1
Mayko2003 <maykox34f@gmail.com>
2023-01-10 22:11:54 -0300

    chore: add activate route, change route for create user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,10 +4,11 @@ const UserController = require('../controllers/user.controller')
 const router = express.Router();
 
 router.get('/', UserController.getUsers)
-router.get('/:username', UserController.getUser)
+router.get('/create/:username', UserController.getUser)
 router.post('/', UserController.createUser)
 router.put('/:username', UserController.updateUser)
 router.delete('/:username', UserController.deleteUser)
 router.put('/restore/:username', UserController.restoreUser)
+router.get('/active', UserController.getActiveUsers)
 
 module.exports = router;
