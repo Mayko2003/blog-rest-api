af95a60008af55848cd93d86de805a76208e8179
Mayko2003 <maykox34f@gmail.com>
2023-01-22 22:53:31 -0300

    chore: add index for utils folder

---
+++ utils/index.js
@@ -0,0 +1,10 @@
+
+const utils = {
+    removeFileExtension: require('./removeFileExtension'),
+    handleValidator: require('./handleValidator'),
+    loadEnvironment: require('./loadEnvironment'),
+    handlePassword: require('./handlePassword'),
+    handleHttpError: require('./handleHttpError'),
+}
+
+module.exports = utils
