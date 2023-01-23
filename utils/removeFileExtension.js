5854bc8116223bd5661b6a5480b1362b00612f66
Mayko2003 <maykox34f@gmail.com>
2023-01-22 22:54:38 -0300

    doc: add jsdoc to removeFileExtension util

--- utils/removeFileExtension.js
+++ utils/removeFileExtension.js
@@ -1,3 +1,8 @@
+/**
+ * Remove the extension from a file name
+ * @param {string} fileName the name of the file
+ * @returns {string} the name of the file without the extension
+ */
 const removeExtension = (fileName)=>{
     return fileName.split('.').shift();
 }
