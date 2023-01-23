1744ec5f5fa34b2bba37fd492ced02b21c2cc978
Mayko2003 <maykox34f@gmail.com>
2023-01-22 22:52:25 -0300

    doc: add comments to every route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const UserController = require('../controllers/user.controller')
-
+const { UserController } = require('../controllers')
+const { UserValidator } = require('../validators')
 const router = express.Router();
 
+// get all users
 router.get('/', UserController.getUsers)
-router.get('/create/:username', UserController.getUser)
-router.post('/', UserController.createUser)
+// get an user (default search field is username)
+router.get('/find', UserController.getUser)
+// create user
+router.post('/', UserValidator.createUser, UserController.createUser)
+// update user
 router.put('/:username', UserController.updateUser)
+// delete user
 router.delete('/:username', UserController.deleteUser)
+// restore user
 router.put('/restore/:username', UserController.restoreUser)
+// get active users
 router.get('/active', UserController.getActiveUsers)
 
 module.exports = router;
